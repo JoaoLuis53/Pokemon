3a97a7b41d5702b1e8b452a19a3bf53dc4d279ef
JoaoLuis53 <joaoluisborgeslima@gmail.com>
2023-02-25 16:29:18 -0300

    Enviando Projeto

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  },
+
   swcMinify: true,
 }
+
 
 module.exports = nextConfig
